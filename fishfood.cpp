ca440a17798507752b85150139b2948b775f55ce
Fan Pu <zengfanpu@gmail.com>
2015-01-09 22:44:15 +0800

    adjlist for fishfood

--- fishfood.cpp
+++ fishfood.cpp
@@ -1,8 +1,18 @@
 #include "fishfood.h"
 #include <cstdio>
 #include <iostream>
+#define mp make_pair
 using namespace std;
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+typedef vector<vii> adjList;
+adjList al;
 
 int fishfood(int N, int E, int K, int A[], int B[], int T[]) {
-    return 0;
+	al.assign(N,vii());
+	for(int i = 0; i < E; i++){
+		al[A[i]].push_back(mp(B[i],T[i]));
+		al[B[i]].push_back(mp(A[i],T[i]));
+	}
+	return 0;
 }
