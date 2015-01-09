f3bd6a4a08b66bbc753508d7822b827b53bf6dde
Fan Pu <zengfanpu@gmail.com>
2015-01-09 23:14:51 +0800

    completed dijkstra

--- fishfood.cpp
+++ fishfood.cpp
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <iostream>
 #define mp make_pair
+#define INF 2000000000
 using namespace std;
 typedef pair<int,int> ii;
 typedef vector<ii> vii;
@@ -14,5 +15,29 @@ int fishfood(int N, int E, int K, int A[], int B[], int T[]) {
 		al[A[i]].push_back(mp(B[i],T[i]));
 		al[B[i]].push_back(mp(A[i],T[i]));
 	}
-	return 0;
+	int dist[N]; //dist from source
+	for(int i = 0; i < N; i++) dist[i] = INF;
+	priority_queue<ii, vii, greater<ii> > pq; pq.push(ii(0,0)); //weight, label
+	while(!pq.empty()){
+		ii front = pq.top(); pq.pop();
+		int d = front.first, u = front.second;
+		if(d > dist[u]) continue;
+		for(int j = 0; j < (int)al[u].size(); j++){
+			ii v = al[u][j][;
+			if(dist[u] + v.second < dist[v.first]){
+				dist[v.first] = dist[u] + v.second;
+				pq.push(ii(dist[v.first], v.first));
+			}
+		}
+	}
+	sort(dist,dist+N);
+	return dist[K-1];
+}
+
+int main() {
+	int N, E, K, A[500000], B[500000], T[500000];
+    scanf("%d%d%d", &N, &E, &K);
+    for ( int i = 0; i < E; i++ ) scanf("%d%d%d", &A[i], &B[i], &T[i]);
+    printf("%d\n", fishfood(N, E, K, A, B, T));
+    return 0;
 }
--- grader.cpp
+++ grader.cpp
@@ -3,9 +3,9 @@
 #include <iostream>
 using namespace std;
 
-static int N, E, K, A[500000], B[500000], T[500000];
 
 int main() {
+	int N, E, K, A[500000], B[500000], T[500000];
     scanf("%d%d%d", &N, &E, &K);
     for ( int i = 0; i < E; i++ ) scanf("%d%d%d", &A[i], &B[i], &T[i]);
     printf("%d\n", fishfood(N, E, K, A, B, T));
