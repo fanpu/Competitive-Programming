fd4f4dc498d9d040431566843a9b979dae12d32e
Fan Pu <zengfanpu@gmail.com>
2015-01-09 22:21:35 +0800

    Added grader.cpp

---
+++ grader.cpp
@@ -0,0 +1,13 @@
+#include "fishfood.h"
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+static int N, E, K, A[500000], B[500000], T[500000];
+
+int main() {
+    scanf("%d%d%d", &N, &E, &K);
+    for ( int i = 0; i < E; i++ ) scanf("%d%d%d", &A[i], &B[i], &T[i]);
+    printf("%d\n", fishfood(N, E, K, A, B, T));
+    return 0;
+}
