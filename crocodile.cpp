c7ea340341b6d48e667a78f7ee2f1cfa469e05ea
Fan Pu <zengfanpu@gmail.com>
2015-01-26 20:10:30 +0800

    created crocodile.cpp

---
+++ crocodile.cpp
@@ -0,0 +1,14 @@
+#include "crocodile.h"
+
+int travel_plan(int N, int M, int R[][2], int L[], int K, int P[])
+{
+	// N: number of chambers
+	// M: number of corridors
+	// R: corridor array
+	// L: time require to run thru corridor
+	// K: num of exit
+	// P: exit chambers array
+  return N;
+}
+
+
