344d6b376e843d7ec1f0090c33b611e7acf891fb
Fan Pu <nerraruzi@users.noreply.github.com>
2015-01-26 16:48:29 +0800

    Update street.cpp
    AC solution

--- street.cpp
+++ street.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 int main()
 {   
-    //freopen("in.txt","r",stdin);
     int n,k,t; cin >> n >> k >> t;
     int restriction[n+1];
     for(int i = 1; i <= n; i++) scanf("%d",&restriction[i]);
@@ -15,20 +14,14 @@ int main()
             int minHeight = restriction[i];
             int maxArea = 0;
             for(int a = 0; a < i  && a < t; a++){
-            	cout << "i is " << i << " and j is " << j << endl;
                 minHeight = min(minHeight, restriction[i-a]);
                 maxArea = max(maxArea, minHeight*(a+1) + dp[i-a-1][j-1]);
-                cout << "current minheight is " << minHeight << " and maxarea is " << maxArea << endl;
             }
             dp[i][j] = max(maxArea, dp[i-1][j]);
         }
         
     }
-    for(int i = 1; i <= n; i++){
-   		for(int j = 1; j <= k; j++){
-   			cout << dp[i][j] << " ";
-   		}cout << endl;
-   	}
+
     cout << dp[n][k];
    return 0;
 }
