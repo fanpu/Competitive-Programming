a6041727407311e1093b40655578962a42f18d97
Fan Pu <zengfanpu@gmail.com>
2015-01-26 18:22:09 +0800

    AC solution for stones

--- stones.cpp
+++ stones.cpp
@@ -13,20 +13,17 @@ int dfs(int curNode){
 	if(al[curNode].size() == 0){
 		return stoneArr[curNode] - 1;
 	}
-	cout << "hi2\n";
-	cin >> test;
 	for(int i = 0; i < (int)al[curNode].size(); i++){
 		int temp = dfs(al[curNode][i]);
 		moves += abs(temp);
 		numStones += temp;
 	}
-	cout << "hi\n";
-	cin >> test;
 	numStones += stoneArr[curNode];
 	return numStones - 1;
 }
 
 int main() {
+	//freopen("input.txt","r",stdin);
 	int n; cin >> n;
 	al.assign(n+1,vi());
 	int rootNode, rootStones;
@@ -34,15 +31,13 @@ int main() {
 	for(int i = 0; i < n; i++){
 		scanf("%d %d", &parent, &stones);
 		if(parent==-1){
-			rootNode = parent;
+			rootNode = i;
 			stoneArr[i] = stones;
 			continue;
 		}
 		al[parent].push_back(i);
 		stoneArr[i] = stones;
 	}
-	cout << "lel";
-	cin >> n;
 	for(int i = 0; i < (int)al[rootNode].size(); i++){
 		moves += abs(dfs(al[rootNode][i]));
 	}
